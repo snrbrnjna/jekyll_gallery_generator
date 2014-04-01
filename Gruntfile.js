cd847db01516413653ca55e17cc151fe35ddafc0
Matthias Thar <matthias.thar@gmail.com>
2014-04-01 18:31:25 +0200

    chore: added grunt workflow for dev and dist
    - serve:
      build dev version (jekyll & copy src) and serve  with livereload on any change
    - build:
      build dist version (jekyll & usemin)
    - serve:dist
      build dist version and serve it without livereload

---
+++ Gruntfile.js
@@ -0,0 +1,118 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // configurable paths
+  var cfg = {
+    dist: '_site'
+  };
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cfg: cfg,
+    watch: {
+      dev: {
+        options: {
+          livereload: 35729
+        },
+        files: [
+          '*.{html,md,markdown}',
+          '_layouts/*.html',
+          '_posts/**/*.{markdown,md}',
+          '_plugins/**/*.rb',
+          '_includes/**/*.{html}',
+          'js/**/*.js',
+          'css/**/*.*',
+          'bower_components/**/*.{js,css}'
+        ],
+        tasks: ['build:dev']
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '0.0.0.0',
+        base: '<%= cfg.dist %>',
+        open: true
+      },
+      server: {
+        options: {}
+      },
+      livereload: {
+        options: {
+          livereload: 35729,
+        }
+      }
+    },
+    copy: {
+      dev: {
+        // on dist build the bower components are moved by usemin
+        // on dev target, we have to copy them by hand
+        files: [
+          {
+            src: [
+              'bower_components/normalize.css/normalize.css',
+              'bower_components/jquery/dist/jquery.*'
+            ],
+            dest: '<%= cfg.dist %>/'
+          }
+        ]
+      }
+    },
+    useminPrepare: {
+      options: {
+        dest: '<%= cfg.dist %>',
+        root: '.' // else the sources are searched in cfg.dist folder
+      },
+      html: '<%= cfg.dist %>/index.html',
+    },
+    usemin: {
+      options: {
+        assetsDirs: '<%= cfg.dist %>',
+      },
+      html: ['<%= cfg.dist %>/**/*.html'],
+      css: ['<%= cfg.dist %>/css/**/*.css']
+    },
+    jekyll: {
+      options: {
+        bundleExec: true
+      },
+      build: {}
+    }
+  });
+
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run([ 'build', 'connect:server:keepalive' ]);
+    }
+
+    grunt.task.run([
+      'build:dev',
+      'connect:livereload',
+      'watch:dev'
+    ]);
+  });
+
+  grunt.registerTask('build', function (target) {
+    if (target === 'dev') {
+      return grunt.task.run([ 'jekyll:build', 'copy:dev' ]);
+    }
+
+    grunt.task.run([
+      'jekyll:build',
+      'useminPrepare',
+      'concat',
+      'cssmin',
+      'uglify',
+      'usemin'
+    ]);
+  });
+
+  grunt.registerTask('default', [
+    'serve'
+  ]);
+};
